0c1e35048286d1f3d60fb60739c90a4cf3870752
anonymous <44284744+objectMaker@users.noreply.github.com>
2022-03-13 07:57:02 +0800

    修改服务器配置

--- src/testServer/index.js
+++ src/testServer/index.js
@@ -9,6 +9,13 @@ app.get('/test',(req,res)=>{
         res.send('你的名字不是abc')
     }
 })
+app.get('/bc',(req,res)=>{
+    if(req.query.name = '小红'){
+        res.send('赤饭睡觉')
+    }else{
+        res.send('你不是小红')
+    }
+})
 app.listen('3001',function (){
     console.log('服务器启动成功');
 })
